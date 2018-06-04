4e6a2e53d7c6e6e1dcde2f1573039fbe1eef30fc
Juan <juanbedon26@hotmail.com>
2018-06-04 13:21:15 -0500

    Funcionalidad ejercicio

--- src/App.js
+++ src/App.js
@@ -5,24 +5,53 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.filter = this.filter.bind(this);
+    this.state = { initialPosts: posts, filterPosts: [] }
+  }
+
   render() {
+    let datos = this.state.data;
+    if(this.state.filter){
+      datos = datos.filter (post =>
+      post.title.toLowerCase()
+      .includes(this.state.filter.toLowerCase()))
+    }
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input onChange={this.filter} type="text" placeholder="Ingresa el término de búsqueda" />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
-        </ul>
+          {this.state.filterPosts.map((post, index) =>
+            <li key={index}>
+              <a href={post.url}><img src={post.image } alt={post.title} /></a>
+              <p>{ post.title }</p>
+            </li>
+          )}
+          </ul>
       </div>
     )
   }
+
+
+  componentWillMount() {
+    this.setState({filterPosts: this.state.initialPosts})
+  }
+
+  filter(e) {
+    let oldPosts = this.state.initialPosts;
+    oldPosts = oldPosts.filter(function(post) {
+      return post.title.toLowerCase().search(
+      e.target.value.toLowerCase()) !== -1;
+    })
+    this.setState({
+      filterPosts: oldPosts
+    })  }
+
 }
 
 
 export default App
-
-
